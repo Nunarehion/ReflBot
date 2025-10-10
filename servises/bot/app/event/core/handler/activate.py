0184832d051684da120c569216dfb32f23b6e4b7
Nunarehion <145133685+Nunarehion@users.noreply.github.com>
2025-10-10 17:39:57 +0500

    Update activate.py

--- servises/bot/app/event/core/handler/activate.py
+++ servises/bot/app/event/core/handler/activate.py
@@ -3,9 +3,15 @@ from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from app.messages.message import send_message
 from app.database.service import DatabaseService
+from app.event.function.activate import activate_user
 
 router = Router()
 
 @router.message(Command("activate"))
 async def start_command(message: types.Message, state: FSMContext, db_service: DatabaseService):
- ...
+    await state.clear()
+    result = await activate_user_from_message_by_tg_id(message, db_service)
+    if result is None:
+        await send_message(message, "activate_not_found")
+        return
+    await send_message(message, "activate_11")
