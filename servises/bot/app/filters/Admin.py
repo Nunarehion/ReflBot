f6cb948fc4e17c17ed2024375e9ef320790bda45
Nunarehion <145133685+Nunarehion@users.noreply.github.com>
2025-10-10 14:00:47 +0500

    Create filters/Admin.py

---
+++ servises/bot/app/filters/Admin.py
@@ -0,0 +1,15 @@
+from aiogram.filters import BaseFilter
+from aiogram.types import Message
+from typing import Optional
+
+
+class DBAdminFilter(BaseFilter):
+    def __init__(self, db_service: DatabaseService):
+        self.db_service = db_service
+
+    async def __call__(self, message: Message) -> bool:
+        if not message.from_user:
+            return False
+
+        user = await self.db_service.get_user_by_id(message.from_user.id)
+        return bool(user and user.get("is_admin"))
