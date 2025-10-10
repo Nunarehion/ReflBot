d78cd0f9f4f5d476c0b4d15e2b2c8533322cb66c
Nunarehion <145133685+Nunarehion@users.noreply.github.com>
2025-10-10 14:15:00 +0500

    Update Admin.py

--- servises/bot/app/filters/Admin.py
+++ servises/bot/app/filters/Admin.py
@@ -1,15 +1,27 @@
-# app/filters.py
 from aiogram.filters import BaseFilter
 from aiogram.types import Message
 
 class DBAdminFilter(BaseFilter):
+    def __init__(self, required_access: str | None = None):
+        self.required_access = required_access
+
     async def __call__(self, message: Message) -> bool:
         if not message.from_user:
             return False
- 
-        dispatcher = message.get_bot().dispatcher
-        db_service = dispatcher.get('db_service')
+
+        dispatcher = getattr(message.bot, "dispatcher", None)
+        if not dispatcher:
+           return False
+
+        db_service = dispatcher.get("db_service")
         if not db_service:
             return False
-        user = await db_service.get_user_by_id(message.from_user.id)
-        return bool(user and user.get("is_admin"))
+            
+        admin = await db_service.get_admin_by_telegram_id(message.from_user.id)
+        if not admin:
+            return False
+
+        if self.required_access and admin.get("access_level") != self.required_access:
+            return False
+
+        return True
